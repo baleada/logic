95a47c7665910be64ed9644dacad0da90d9a9bf7
AlexVipond <alex.r.vipond@gmail.com>
2019-08-31 23:38:01 -0600

    Empty test fixtures during browserify step

--- scripts/browserifyLibraries.js
+++ scripts/browserifyLibraries.js
@@ -10,8 +10,16 @@ function browserifyLibraries() {
       output: `./tests/fixtures/libraries/${library}`,
     }))
 
+  empty()
   libraries.forEach(library => browserifyLibrary(library))
   console.log(`Browserified ${libraries.length} libraries.`)
+}
+
+function empty() {
+  fs.readdirSync('./tests/fixtures/libraries')
+    .forEach(item => fs.unlinkSync(`./tests/fixtures/libraries/${item}`))
+
+  console.log('Emptied library fixtures')
 }
 
 function browserifyLibrary(library) {
