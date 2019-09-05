c9e0e066c1b772561387ae1ea951e13b8cd65c61
AlexVipond <alex.r.vipond@gmail.com>
2019-09-05 11:36:11 -0600

    fix linting errors

--- scripts/buildIndices.js
+++ scripts/buildIndices.js
@@ -1,20 +1,19 @@
 const fs = require('fs')
 
-function buildIndices() {
+function buildIndices () {
   buildLibraryIndex()
   buildHelperIndex()
 }
 
-function buildLibraryIndex() {
+function buildLibraryIndex () {
   const libraries = fs
-    .readdirSync('./src/libraries')
-    .map(library => ({
-      path: `./libraries/${library}`,
-      name: library.split('.')[0],
-    }))
-
-  const imported = libraries.reduce((imported, library) => `${imported}import ${library.name} from '${library.path}'\n`, '')
-  const exported = libraries.reduce((exported, library) => `${exported}  ${library.name},\n`, 'export default {\n') + '}'
+          .readdirSync('./src/libraries')
+          .map(library => ({
+            path: `./libraries/${library}`,
+            name: library.split('.')[0],
+          })),
+        imported = libraries.reduce((imported, library) => `${imported}import ${library.name} from '${library.path}'\n`, ''),
+        exported = libraries.reduce((exported, library) => `${exported}  ${library.name},\n`, 'export {\n') + '}'
 
   fs.writeFileSync(
     './src/index.js',
@@ -24,16 +23,15 @@ function buildLibraryIndex() {
   console.log(`Indexed ${libraries.length} libraries.`)
 }
 
-function buildHelperIndex() {
+function buildHelperIndex () {
   const helpers = fs
-    .readdirSync('./src/helpers')
-    .map(helper => ({
-      path: `./${helper}`,
-      name: helper.split('.')[0],
-    }))
-
-  const imported = helpers.reduce((imported, helper) => `${imported}import ${helper.name} from '${helper.path}'\n`, '')
-  const exported = helpers.reduce((exported, helper) => `${exported}  ${helper.name},\n`, 'export default {\n') + '}'
+          .readdirSync('./src/helpers')
+          .map(helper => ({
+            path: `./${helper}`,
+            name: helper.split('.')[0],
+          })),
+        imported = helpers.reduce((imported, helper) => `${imported}import ${helper.name} from '${helper.path}'\n`, ''),
+        exported = helpers.reduce((exported, helper) => `${exported}  ${helper.name},\n`, 'export {\n') + '}'
 
   fs.writeFileSync(
     './src/helpers/index.js',
