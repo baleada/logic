014310a7d2b19c3b5d4e2a521f98a5d39f5eb4de
AlexVipond <alex.r.vipond@gmail.com>
2019-09-10 23:02:14 -0600

    Tweak SyncableMap and SyncableObject tests, test SyncableArray

--- tests/libraries/SyncableMap.test.js
+++ tests/libraries/SyncableMap.test.js
@@ -23,9 +23,9 @@ test('editableState is state', t => {
 
 /* Methods */
 test('write({ key, rename, value }) emits new state with rename deleted and new key set to value through onSync', t => {
-  let value
+  let result
   const instance = t.context.setup({
-    onSync: newState => (value = newState)
+    onSync: newState => (result = newState)
   })
 
   instance.write({
@@ -34,13 +34,13 @@ test('write({ key, rename, value }) emits new state with rename deleted and new 
     value: 'Baleada Logic',
   })
 
-  t.deepEqual(value, new Map([['newName', 'Baleada Logic'], ['description', 'A toolkit for building web apps']]))
+  t.deepEqual(result, new Map([['newName', 'Baleada Logic'], ['description', 'A toolkit for building web apps']]))
 })
 
 test('write({ key, rename }) emits new state with rename renamed to key through onSync', t => {
-  let value
+  let result
   const instance = t.context.setup({
-    onSync: newState => (value = newState)
+    onSync: newState => (result = newState)
   })
 
   instance.write({
@@ -48,13 +48,13 @@ test('write({ key, rename }) emits new state with rename renamed to key through 
     rename: 'name',
   })
 
-  t.deepEqual(value, new Map([['newName', 'Baleada'], ['description', 'A toolkit for building web apps']]))
+  t.deepEqual(result, new Map([['newName', 'Baleada'], ['description', 'A toolkit for building web apps']]))
 })
 
 test('write({ key, value }) emits new state with key set to value through onSync', t => {
-  let value
+  let result
   const instance = t.context.setup({
-    onSync: newState => (value = newState)
+    onSync: newState => (result = newState)
   })
 
   instance.write({
@@ -62,40 +62,40 @@ test('write({ key, value }) emits new state with key set to value through onSync
     value: 'Baleada Logic',
   })
 
-  t.deepEqual(value, new Map([['name', 'Baleada'], ['description', 'A toolkit for building web apps'], ['newName', 'Baleada Logic']]))
+  t.deepEqual(result, new Map([['name', 'Baleada'], ['description', 'A toolkit for building web apps'], ['newName', 'Baleada Logic']]))
 })
 
 test('erase({ key }) emits new state with key deleted through onSync', t => {
-  let value
+  let result
   const instance = t.context.setup({
-    onSync: newState => (value = newState)
+    onSync: newState => (result = newState)
   })
 
   instance.erase({ key: 'description' })
 
-  t.deepEqual(value, new Map([
+  t.deepEqual(result, new Map([
     ['name', 'Baleada']
   ]))
 })
 
 test('erase({ last }) emits new state with last key deleted through onSync', t => {
-  let value
+  let result
   const instance = t.context.setup({
-    onSync: newState => (value = newState)
+    onSync: newState => (result = newState)
   })
 
   instance.erase({ last: true })
 
-  t.deepEqual(value, new Map([['name', 'Baleada']]))
+  t.deepEqual(result, new Map([['name', 'Baleada']]))
 })
 
 test('erase({ all }) emits new state with all keys deleted through onSync', t => {
-  let value
+  let result
   const instance = t.context.setup({
-    onSync: newState => (value = newState)
+    onSync: newState => (result = newState)
   })
 
   instance.erase({ all: true })
 
-  t.deepEqual(value, new Map())
+  t.deepEqual(result, new Map())
 })
