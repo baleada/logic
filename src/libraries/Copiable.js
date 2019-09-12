358afb2e53cf14ebf5db5b02905fb3904d31e142
AlexVipond <alex.r.vipond@gmail.com>
2019-09-11 18:27:08 -0600

    write Copiable

--- src/libraries/Copiable.js
+++ src/libraries/Copiable.js
@@ -7,27 +7,97 @@
 /* Dependencies */
 
 /* Utils */
+import warn from '../utils/warn'
 
 export default class Copiable {
-  /* Private properties */
+  #computedClipboard
+  #computedSucceeded
+  #computedErrored
 
-  constructor(state, options = {}) {
+  constructor (string, options = {}) {
     /* Options */
+    options = {
+      usesFallbacks: false,
+      ...options,
+    }
+    this.#usesFallbacks = options.usesFallbacks
+    this.#onRead = options.onRead
 
     /* Public properties */
-    this.state = state
+    this.string = string
 
+    /* Private properties */
+    this.#computedClipboard = navigator.clipboard
+    this.#computedSucceeded = false
+    this.#computedErrored = false
     /* Dependency */
   }
 
   /* Public getters */
+  get clipboard () {
+    return this.#computedClipboard
+  }
+  get copied () {
+    return this.#getCopied
+  }
+  get succeeded () {
+    return this.#computedSucceeded
+  }
+  get errored () {
+    return this.#computedErrored
+  }
 
   /* Public methods */
-  setState(state) {
-    this.state = state
+  setString (string) {
+    this.string = string
+    return this
+  }
+  copy () {
+    if (this.#usesFallbacks) {
+      this.#writeTextFallback()
+      this.#computedErrored = false
+      this.#computedSucceeded = true
+    } else {
+      this.#writeText()
+        .then(() => {
+          this.#computedErrored = false
+          this.#computedSucceeded = true
+        })
+        .catch(() => {
+          this.#computedErrored = true
+          this.#computedSucceeded = false
+        })
+    }
+
     return this
   }
 
   /* Private methods */
+  #getCopied = function() {
+    if (this.#usesFallbacks) {
+      warn('noFallbackAvailable', {
+        subject: 'Copiable\'s copied property'
+      })
+    } else {
+      return this.#readText()
+        .then(text => text)
+    }
+  }
+  #readText = function() {
+    return this.#clipboard.readText()
+  }
+  #writeText = function() {
+    return this.clipboard.writeText(this.string)
+  }
+  #writeTextFallback = function() {
+    const input = document.createElement('input')
+    input.type = 'text'
+    input.value = this.string
 
+    document.body.appendChild(input)
+    input.select()
+    document.execCommand('copy')
+
+    document.body.removeChild(input)
+  }
 }
