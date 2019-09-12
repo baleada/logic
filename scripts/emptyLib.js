eb355d1bc920d6c247db6c1ad252e26f58da9280
AlexVipond <alex.r.vipond@gmail.com>
2019-09-12 14:30:44 -0600

    lint

--- scripts/emptyLib.js
+++ scripts/emptyLib.js
@@ -1,15 +1,16 @@
 const fs = require('fs')
 
-function emptyLib() {
+function emptyLib () {
   fs.readdirSync('./lib')
     .forEach(item => remove(item))
 
   console.log('Emptied lib directory')
 }
 
-function remove(item) {
-  if (item.includes('.')) fs.unlinkSync(`./lib/${item}`)
-  else {
+function remove (item) {
+  if (item.includes('.')) {
+    fs.unlinkSync(`./lib/${item}`)
+  } else {
     fs.readdirSync(`./lib/${item}`)
       .forEach(file => remove(`${item}/${file}`))
 
