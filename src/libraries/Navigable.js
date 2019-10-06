bd3e160438e706806eba31794199491894d0b7b2
AlexVipond <alex.r.vipond@gmail.com>
2019-10-06 17:37:16 -0600

    Remove Navigable docs

--- src/libraries/Navigable.js
+++ src/libraries/Navigable.js
@@ -4,28 +4,12 @@
  * Released under the MIT license
  */
 
-/**
- * Navigable is a library that enriches an array by:
- * - Allowing it to store a index of the item that has been navigated to
- * - Giving it the methods necessary to navigate to a different item
- *
- * Navigable is written in vanilla JS with no dependencies. It powers <nuxt-link to="/docs/tools/composition-functions/useNavigable">`useNavigable`</nuxt-link>.
- */
 class Navigable {
   #loops
   #increment
   #decrement
   #computedLocation
 
-  /**
-   * Constructs a Navigable instance
-   * @param {Array}  array          The array that will be made navigable
-   * @param {Number}  [initialLocation=0] The default location
-   * @param {Boolean} [loops=true]   `true` when the Navigable instance should loop around to the beginning of the array when it navigates past the last item and loop around to the end when it navigates before the first item. `false` when navigating past the last item or before the first item does not change the location.
-   * @param {Number}  [increment=1]  The number of items that will be traversed when the navigable instance is stepping forward through the array
-   * @param {Number}  [decrement=1]  The number of items that will be traversed when the navigable instance is stepping backward through the array
-   * @param {Function}  onNavigate    A function that Navigable will call after navigating to a new item. `onNavigate` acceepts two parameters: the index-based location (Number) of the item that has been navigated to, and the Navigable instance (Object).
-   */
   constructor (array, options = {}) {
     /* Options */
     options = {
@@ -41,10 +25,6 @@ class Navigable {
     this.#decrement = options.decrement
 
     /* Public properties */
-    /**
-     * A shallow copy of the array passed to the Navigable constructor
-     * @type {Array}
-     */
     this.array = array
 
     /* Private properties */
@@ -59,20 +39,10 @@ class Navigable {
   }
 
   /* Public methods */
-  /**
-   * Sets the Navigable instance's array
-   * @param {Array} array The new array
-   * @return {Object}       The new Navigable instance
-   */
   setArray (array) {
     this.array = array
     return this
   }
-  /**
-   * Navigates to a specific item
-   * @param  {Number} newLocation The index-based location of the item that should be navigated to
-   * @return {Object}       The Navigable instance
-   */
   goTo (newLocation) {
     switch (true) {
     case (newLocation > this.array.length):
@@ -89,10 +59,6 @@ class Navigable {
 
     return this.#navigate(newLocation)
   }
-  /**
-   * Steps forward through the array, increasing `location` by `increment`
-   * @return {Object}       The Navigable instance
-   */
   next () {
     let newLocation
     const lastLocation = this.array.length - 1
@@ -114,10 +80,6 @@ class Navigable {
 
     return this.goTo(newLocation)
   }
-  /**
-   * Steps backward through the array, decreasing `location` by `decrement`
-   * @return {Object}       The Navigable instance
-   */
   prev () {
     let newLocation
 
