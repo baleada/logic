9091cf67672ae34f51013b155edf27b906881fd5
AlexVipond <alex.r.vipond@gmail.com>
2020-03-09 16:34:25 -0600

    Remove docs from code

--- src/classes/Delayable.js
+++ src/classes/Delayable.js
@@ -6,12 +6,6 @@
 
 import Animateable from './Animateable'
 
-/**
- * Delayable is a library that enriches a function by:
- * - Giving it the methods necessary to execute itself after a delay or at regular intervals<
- * - Allowing it to store the time elapsed since it was delayed, the time remaining until it will be executed, and the number of times it has been executed
- * Delayable depends on `[setTimeout](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout)`, `[setInterval](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval)`, `[requestAnimationFrame](https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame)`, and the `[global Data object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date)`, but is otherwise written in vanilla JS. Delayable powers <nuxt-link to="/docs/tools/composition-functions/useDelayable">`useDelayable`</nuxt-link>.
- */
 export default class Delayable {
   constructor (callback, options) {
     options = {
@@ -47,17 +41,9 @@ export default class Delayable {
   get status () {
     return this._computedStatus
   }
-  /**
-   * The number of times the callback function has been executed
-   * @type {Number}
-   */
   get executions () {
     return this._animateable.iterations
   }
-  /**
-   * The time (in milliseconds) that has elapsed since the callback function was initially delayed OR last executed, whichever is smaller
-   * @type {Number}
-   */
   get time () {
     return this._animateable.time
   }
@@ -65,10 +51,6 @@ export default class Delayable {
     return this._animateable.progress.time
   }
 
-  /**
-   * Sets the Delayable instance's callback function
-   * @param {Function} callback The new callback function
-   */
   setCallback (naiveCallback) {
     this.stop()
 
@@ -102,9 +84,6 @@ export default class Delayable {
     this._computedStatus = 'delayed'
   }
 
-  /**
-   * Executes the callback function after the period of time specified by <code>delay</code>
-   */
   delay () {
     switch (this.status) {
     case 'delaying':
@@ -158,7 +137,8 @@ export default class Delayable {
   seek (timeProgress) {
     const executions = Math.floor(timeProgress)
 
-  if (executions > 0) {case '      window':.requestAnimationFrame(timestamp => {
+  if (executions > 0) {
+    window.requestAnimationFrame(timestamp => {
         for (let i = 0; i < executions; i++) {
           this.callback({
             data: { progress: 1 },
@@ -177,9 +157,6 @@ export default class Delayable {
     this._computedStatus = 'sought'
   }
   
-  /**
-   * Stops the delayed callback function. The function won't be executed, but <code>timeElapsed</code> and <code>timeRemaining</code> will <b>not</b> be reset to their initial values.
-   */
   stop () {
     this._animateable.stop()
     this._stopped()
