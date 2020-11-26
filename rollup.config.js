d2e1888342a5cca4a22668b7f9a6b108f519ed20
AlexVipond <alex.r.vipond@gmail.com>
2020-11-26 00:58:09 -0600

    sketching out new test setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,6 @@
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import { readFileSync } from 'fs'
 import { configureable } from '@baleada/prepare'
 import toMetadata from './source-transforms/toMetadata'
 
@@ -25,19 +28,38 @@ const shared = configureable()
         .resolve()
 
 export default [
-  shared
-    .delete({ targets: 'lib/*', verbose: true })
-    .esm({ file: 'lib/index.esm.js', target: 'browser' })
-    .analyze()
-    .configure(),
-  shared
-    .cjs({ file: 'lib/index.js' })
-    .configure(),
-  metadataShared    
-    .delete({ targets: 'metadata/*', verbose: true })
-    .esm({ file: 'metadata/index.esm.js', target: 'node' })
-    .configure(),
-  metadataShared    
-    .cjs({ file: 'metadata/index.js' })
+  // shared
+  //   .delete({ targets: 'lib/*', verbose: true })
+  //   .esm({ file: 'lib/index.esm.js', target: 'browser' })
+  //   .analyze()
+  //   .configure(),
+  // shared
+  //   .cjs({ file: 'lib/index.js' })
+  //   .configure(),
+  // metadataShared    
+  //   .delete({ targets: 'metadata/*', verbose: true })
+  //   .esm({ file: 'metadata/index.esm.js', target: 'node' })
+  //   .configure(),
+  // metadataShared    
+  //   .cjs({ file: 'metadata/index.js' })
+  //   .configure(),
+  configureable()
+    .input('src/index.js')
+    .resolve()
+    .sourceTransform({
+      test: ({ source }) => source === '',
+      transform: ({ id, source }) => {
+        console.log(id)
+        return readFileSync(id, 'utf8')
+      }
+    })
+    .plugin(json())
+    .plugin(commonjs()) //{ requireReturnsDefault: 'auto' }
+    .esm({ file: 'tests/fixtures/index.js', target: 'browser' })
+    .virtualIndex('src/index.js', { test: ({ id }) => /src\/(?:classes|factories)\/\w+.js$/.test(id) })
+    .virtualIndex('src/classes')
+    .virtualIndex('src/constants')
+    .virtualIndex('src/factories')
+    .virtualIndex('src/util')
     .configure(),
 ]
---
+++ tests/node/Navigateable.test.js
@@ -0,0 +1,216 @@
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+import { Navigateable } from '../fixtures/index.js'
+
+test.before.each(context => {
+  context.setup = (options = {}) => new Navigateable(
+    ['tortilla', 'frijoles', 'mantequilla', 'aguacate', 'huevito'],
+    options
+  )
+})
+
+test('stores the array', context => {
+  const instance = context.setup()
+
+  assert.equal(instance.array, ['tortilla', 'frijoles', 'mantequilla', 'aguacate', 'huevito'])
+})
+
+test('assignment sets the array', context => {
+  const instance = context.setup()
+  instance.array = ['Baleada']
+
+  assert.equal(instance.array, ['Baleada'])
+})
+
+test('setArray sets the array', context => {
+  const instance = context.setup()
+  instance.setArray(['Baleada'])
+
+  assert.equal(instance.array, ['Baleada'])
+})
+
+test('initial location is 0 by default', context => {
+  const instance = context.setup()
+
+  assert.is(instance.location, 0)
+})
+
+test('initial location can be customized via options', context => {
+  const instance = context.setup({
+    initialLocation: 1,
+  })
+
+  assert.is(instance.location, 1)
+})
+
+test('assignment sets the location', context => {
+  const instance = context.setup()
+
+  instance.location = 1
+
+  assert.is(instance.location, 1)
+})
+
+test('setLocation sets the location', context => {
+  const instance = context.setup()
+
+  instance.setLocation(1)
+
+  assert.is(instance.location, 1)
+})
+
+/* navigate */
+test('navigate(newLocation) navigates to the last item in the array when newLocation is greater than the index of the last item in the array', context => {
+  const instance = context.setup()
+
+  instance.navigate(42)
+
+  assert.is(instance.location, instance.array.length - 1)
+})
+
+test('navigate(newLocation) navigates to 0 when newLocation is less than 0', context => {
+  const instance = context.setup()
+
+  instance.navigate(-42)
+
+  assert.is(instance.location, 0)
+})
+
+test('navigate(newLocation) navigates to newLocation', context => {
+  const instance = context.setup()
+
+  instance.navigate(1)
+
+  assert.is(instance.location, 1)
+})
+
+/* next */
+test('next() increments the current location by 1 when distance is default', context => {
+  const instance = context.setup()
+
+  instance.next()
+
+  assert.is(instance.location, 1)
+})
+
+test('next() increments the current location by distance when distance is not default', context => {
+  const instance = context.setup()
+
+  instance.next({ distance: 2, })
+
+  assert.is(instance.location, 2)
+})
+
+test('next() loops back through the array by default when the current location is greater than the last location', context => {
+  const instance = context.setup()
+
+  instance.navigate(instance.array.length - 1)
+  instance.next()
+
+  assert.is(instance.location, 0)
+})
+
+test('next() loops continuously through the array by default until current location is less than or equal to the last location', context => {
+  const instance = context.setup()
+
+  instance.next({ distance: 15 })
+
+  assert.is(instance.location, 0)
+})
+
+test('next() stops at the last location when loops is false AND incremented location is greater than the last location', context => {
+  const instance = context.setup()
+
+  instance.navigate(instance.array.length - 1)
+  instance.next({ loops: false })
+
+  assert.is(instance.location, instance.array.length - 1)
+})
+
+/* prev */
+test('previous() decrements the current location by 1 when distance is default', context => {
+  const instance = context.setup({
+    initialLocation: 1
+  })
+
+  instance.previous()
+
+  assert.is(instance.location, 0)
+})
+
+test('previous() decrements the current location by distance when distance is not default', context => {
+  const instance = context.setup({
+    initialLocation: 2,
+  })
+
+  instance.previous({ distance: 2 })
+
+  assert.is(instance.location, 0)
+})
+
+test('previous() loops back through the array by default when the current location is less than 0', context => {
+  const instance = context.setup()
+
+  instance.previous()
+
+  assert.is(instance.location, instance.array.length - 1)
+})
+
+test('previous() loops continuously through the array by default until current location is greater than or equal to 0', context => {
+  const instance = context.setup()
+
+  instance.previous({ distance: 15 })
+
+  assert.is(instance.location, 0)
+})
+
+test('previous() stops at 0 when loops is false AND decremented location is less than 0', context => {
+  const instance = context.setup()
+
+  instance.previous({ loops: false })
+
+  assert.is(instance.location, 0)
+})
+
+test('random() navigates to a random location', context => {
+  const instance = context.setup()
+
+  instance.random()
+
+  assert.ok(instance.location >= 0 && instance.location <= instance.array.length)
+})
+
+/* status */
+test('status is "ready" after construction', context => {
+  const instance = context.setup()
+
+  assert.is(instance.status, 'ready')
+})
+
+test('status is "navigated" after any navigation function is called at least once', context => {
+  const navigate = context.setup(),
+        next = context.setup(),
+        previous = context.setup(),
+        random = context.setup()
+
+  navigate.random()
+  next.random()
+  previous.random()
+  random.random()
+
+  assert.ok([navigate, next, previous, random].every(instance => instance.status === 'navigated'))
+})
+
+/* method chaining */
+test('can method chain', context => {
+  const instance = context.setup(),
+        chained = instance
+          .setArray(['Baleada'])
+          .navigate(42)
+          .next(42)
+          .previous(42)
+
+  assert.ok(chained instanceof Navigateable)
+})
+
+test.run()
