fa34c92851ab3f4009476c48f191440d992fdc92
AlexVipond <alex.r.vipond@gmail.com>
2020-12-12 01:22:23 -0600

    tests arrows

--- tests/node/eventMatchesKeycombo.test.js
+++ tests/node/eventMatchesKeycombo.test.js
@@ -86,6 +86,137 @@ suite(`predicates enter, backspace, tab, and space`, context => {
   }))
 })
 
-
+suite(`predicates arrows`, context => {
+  // arrow
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowUp' },
+    combo: [{ name: 'arrow', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowRight' },
+    combo: [{ name: 'arrow', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowDown' },
+    combo: [{ name: 'arrow', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowLeft' },
+    combo: [{ name: 'arrow', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'A' },
+    combo: [{ name: '!arrow', type: 'arrow' }]
+  }))
+  
+  // vertical
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowUp' },
+    combo: [{ name: 'vertical', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowDown' },
+    combo: [{ name: 'vertical', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowRight' },
+    combo: [{ name: '!vertical', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowLeft' },
+    combo: [{ name: '!vertical', type: 'arrow' }]
+  }))
+  
+  // horizontal
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowRight' },
+    combo: [{ name: 'horizontal', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowLeft' },
+    combo: [{ name: 'horizontal', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowUp' },
+    combo: [{ name: '!horizontal', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowDown' },
+    combo: [{ name: '!horizontal', type: 'arrow' }]
+  }))
+  
+  
+  // up
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowUp' },
+    combo: [{ name: 'up', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowRight' },
+    combo: [{ name: '!up', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowDown' },
+    combo: [{ name: '!up', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowLeft' },
+    combo: [{ name: '!up', type: 'arrow' }]
+  }))
+  
+  // right
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowUp' },
+    combo: [{ name: '!right', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowRight' },
+    combo: [{ name: 'right', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowDown' },
+    combo: [{ name: '!right', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowLeft' },
+    combo: [{ name: '!right', type: 'arrow' }]
+  }))
+  
+  // down
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowUp' },
+    combo: [{ name: '!down', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowRight' },
+    combo: [{ name: '!down', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowDown' },
+    combo: [{ name: 'down', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowLeft' },
+    combo: [{ name: '!down', type: 'arrow' }]
+  }))
+  
+  // left
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowUp' },
+    combo: [{ name: '!left', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowRight' },
+    combo: [{ name: '!left', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowDown' },
+    combo: [{ name: '!left', type: 'arrow' }]
+  }))
+  assert.ok(eventMatchesKeycombo({
+    event: { key: 'ArrowLeft' },
+    combo: [{ name: 'left', type: 'arrow' }]
+  }))
+})
 
 suite.run()
