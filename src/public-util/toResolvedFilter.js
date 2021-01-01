89547b83a2f9c40786b208b70b9ad1c6f365e1ea
AlexVipond <alex.r.vipond@gmail.com>
2021-01-01 11:50:05 -0600

    makes toResolvedMap api consistent with toResolvedFilter

--- src/public-util/toResolvedFilter.js
+++ src/public-util/toResolvedFilter.js
@@ -1,6 +1,6 @@
 import toResolvedMap from './toResolvedMap.js'
 
 export default async function toResolvedFilter ({ array, condition }) {
-  const filterResults = await toResolvedMap(() => array.map(async item => await condition(item)))
+  const filterResults = await toResolvedMap({ array, map: condition })
   return array.filter((_, index) => filterResults[index])
 }
--- src/public-util/toResolvedMap.js
+++ src/public-util/toResolvedMap.js
@@ -1,6 +1,6 @@
 import Resolveable from '../classes/Resolveable.js'
 
-export default async function toResolvedMap (getMappedPromises) {
-  const resolveable = new Resolveable(getMappedPromises)
+export default async function toResolvedMap ({ array, map }) {
+  const resolveable = new Resolveable(() => array.map(async item => map(item))))
   return (await resolveable.resolve()).response
 }
