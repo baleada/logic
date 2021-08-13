3dcc379dac6b051ccfd8ca4ca763c8dff829dcd5
AlexVipond <alex.r.vipond@gmail.com>
2021-08-13 17:56:59 -0500

    externalize lazy collections

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,6 +6,7 @@ const external = [
         'fast-fuzzy',
         '@sindresorhus/slugify',
         'dompurify',
+        'lazy-collections',
         /@babel\/runtime/,
       ],
       shared = new configureable.Rollup()
