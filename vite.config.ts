30ad696437f961c34bf25742715c8305a6cf49e0
Alex Vipond <alex.vipond@betterhelp.com>
2023-03-12 22:52:38 -0500

    properly support types in tests

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { configureable } from '@baleada/prepare'
 import { resolve } from 'path'
 
 export default new configureable.Vite()
-  .alias({
-    '@src': `/src`,
-  })
   .rollup(({ configureable }) => configureable
     .input({
       main: resolve(__dirname, 'tests/stubs/app/index.html'),
@@ -13,4 +10,5 @@ export default new configureable.Vite()
   )
   .pages({ pagesDir: 'tests/stubs/app/src/pages' })
   .vue()
+  .inspect()
   .configure()
