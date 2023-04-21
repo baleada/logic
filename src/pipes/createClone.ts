8e2b8aff0c140f136cee14750e16e60c2b4e162b
Alex Vipond <hello@alexvipond.dev>
2023-04-21 00:26:48 -0500

    new pipes

--- src/pipes/createClone.ts
+++ src/pipes/createClone.ts
@@ -2,15 +2,8 @@
 import { klona } from 'klona'
 import type { AnyFunction } from './types'
 
-export function createClone<Any> (kind: 'serializable' | 'deep' = 'serializable'): AnyFunction<Any> {
-  switch (kind) {
-    case 'serializable':
-      return any => {
-        return JSON.parse(JSON.stringify(any))
-      }
-    case 'deep':
-      return any => {
-        return klona(any)
-      }
+export function createClone<Any> (): AnyFunction<Any> {
+  return any => {
+    return klona(any)
   }
 }
